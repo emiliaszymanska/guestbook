8b2ed58c27c7e072d4b1b97d6b1b836173384838
Emilia Szymanska <esz.emilia@gmail.com>
2020-10-12 23:21:35 +0200

    Remove unnecessary spaces

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -8,13 +8,12 @@ import java.net.InetSocketAddress;
 
 public class App {
 
-    public static void main( String[] args ) throws IOException {
+    public static void main(String[] args) throws IOException {
 
         HttpServer server = HttpServer.create(new InetSocketAddress(8008), 0);
 
         server.createContext("/guestbook", new GuestbookHandler());
         server.setExecutor(null);
-
         server.start();
 
         System.out.println("Server started at " + server.getAddress().getPort());
