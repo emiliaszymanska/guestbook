9f931244363d5a81314d61447b97db734281200d
Emilia Szymanska <esz.emilia@gmail.com>
2020-10-14 15:59:05 +0200

    Change port number

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -10,7 +10,7 @@ public class App {
 
     public static void main(String[] args) throws IOException {
 
-        HttpServer server = HttpServer.create(new InetSocketAddress(8008), 0);
+        HttpServer server = HttpServer.create(new InetSocketAddress(8015), 0);
 
         server.createContext("/guestbook", new GuestbookHandler());
         server.setExecutor(null);
